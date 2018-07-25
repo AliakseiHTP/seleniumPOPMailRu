b885f0e589146eccb1a7b0417300e3b8e4f20662
aliaksei.nips <kazakaliaksei@gmail.com>
2018-07-25 21:03:18 +0300

    class 'DriverSingletod' added

---
+++ src/test/java/by/htp/driver/DriverSingleton.java
@@ -0,0 +1,40 @@
+package by.htp.driver;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverSingleton {
+	private static WebDriver driver;
+    private static final Logger logger = LogManager.getRootLogger();
+    private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
+    private static final String CHROME_DRIVER_EXE_PATH = "src//test//resources//chromedriver.exe";
+
+    private DriverSingleton(){};
+
+
+    public static WebDriver getDriver(){
+        if (null == driver){
+            System.setProperty(WEBDRIVER_CHROME_DRIVER, CHROME_DRIVER_EXE_PATH);
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments(Arrays.asList("--incognito", "--start-maximized","--lang=ru"));
+            driver = new ChromeDriver(chromeOptions);
+            driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            //driver.manage().window().maximize();
+            logger.info("Browser started");
+        }
+
+        return driver;
+    }
+
+    public static void closeDriver(){
+        driver.quit();
+        driver = null;
+    }
+}
